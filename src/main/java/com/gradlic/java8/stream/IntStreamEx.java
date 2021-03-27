ca07f2604e90a6497896e363ca2afd11a088f0f8
Vikas Kumar <v.ray96610@gmail.com>
2021-03-27 19:19:30 +0530

    IntStreamEx different example

--- src/main/java/com/gradlic/java8/stream/IntStreamEx.java
+++ src/main/java/com/gradlic/java8/stream/IntStreamEx.java
@@ -1,10 +1,22 @@
 package com.gradlic.java8.stream;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class IntStreamEx {
     public static void main(String[] args) {
+
         IntStream.range(1, 10).forEach(System.out::print);
         System.out.println();
+
+        // Integer stream with skip
+        IntStream.range(1, 10).skip(5).forEach(x-> System.out.println(x));
+        System.out.print("-------------------");
+
+        System.out.print("--------Intstream with sum-----------");
+        System.out.println(IntStream.range(1,5).sum());
+        System.out.println();
+
+
     }
 }

b79fb3af6f39c6b7ca5b6aa7c46dbb18b2056b9b
Vikas Kumar <v.ray96610@gmail.com>
2021-03-27 21:17:14 +0530

    Some more stream example

--- src/main/java/com/gradlic/java8/stream/OtherStreamDemo.java
+++ src/main/java/com/gradlic/java8/stream/OtherStreamDemo.java
@@ -1,11 +1,17 @@
 package com.gradlic.java8.stream;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class OtherStreamDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Stream.of sorted and find first
         Stream.of("Vikas", "Suraj", "Supriya")
                 .sorted()
@@ -30,6 +36,51 @@ public class OtherStreamDemo {
         peoples.stream().map(String::toLowerCase)
                 .filter(x->x.startsWith("h")).forEach(System.out::println);
 
+
+        // Stream rows from text file, sort, filter and print
+        Stream<String> bands = Files.lines(Paths.get("names.txt"));
+        bands.sorted()
+                .filter(x-> x.length() > 3)
+                .forEach(System.out::println);
+
+        bands.close();
+
+        List<String> bands2 = Files.lines(Paths.get("names.txt"))
+                .filter(x->x.contains("df"))
+                .collect(Collectors.toList());
+        bands2.forEach(x-> System.out.println(x));
+
+        // stream rows from csv file
+        Stream<String> rows1 = Files.lines(Paths.get("data.txt"));
+        int rowCount = (int)rows1.map(x->x.split(",")).filter(x->x.length==3).count();
+        System.out.println(rowCount+"rows");
+        rows1.close();
+
+
+
+        Stream<String> rows2 = Files.lines(Paths.get("data.txt"));
+        rows2.map(x->x.split(","))
+                .filter(x -> x.length == 3)
+                .filter(x-> Integer.parseInt(x[1])>15)
+                .forEach(x-> System.out.println(x[0]+" "+x[1]+" "+x[2]));
+
+        rows2.close();
+
+
+        Stream<String> rows3 = Files.lines(Paths.get("data.txt"));
+        Map<String, Integer> map = new HashMap<>();
+        map = rows3.map(x->x.split(","))
+                .filter(x-> x.length == 3)
+                .filter(x-> Integer.parseInt(x[1])> 15)
+                .collect(Collectors.toMap(
+                        x->x[0],
+                        x->Integer.parseInt(x[1])
+                ));
+        rows3.close();
+
+        for (String key: map.keySet()){
+            System.out.println(key+ " "+map.get(key));
+        }
 
 
 
